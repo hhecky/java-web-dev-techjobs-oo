c0ce7b9d617e6f3559e9f0772e5f41e4c1c8529f
Heather Hecky <hhecky@gmail.com>
2020-09-15 15:59:59 -0500

    Assignment 2 done half way thru part 4, finished thru Test the Full Constructor

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,48 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class JobTest {
+    Job test_job1;
+    Job test_job2;
+
+    @Before
+    public void createJobObject() {
+        test_job1 = new Job();
+        test_job2 = new Job();
+    }
+
+    @Test
+    public void testSettingJobId() {
+        assertTrue((test_job1.getId() != test_job2.getId()) && (test_job2.getId() - test_job1.getId() == 1));
+        //System.out.println(test_job1.getId());
+        //System.out.println(test_job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job test_job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals("Product tester", test_job3.getName());
+        System.out.println(test_job3.getName());
+        assertTrue("ACME" == test_job3.getEmployer().toString());
+        System.out.println(test_job3.getEmployer());
+        //test instanceof? to make sure class is employer
+        assertTrue(true == test_job3.getEmployer() instanceof Employer);
+        assertTrue("Desert" == test_job3.getLocation().toString());
+        assertTrue(true == test_job3.getLocation() instanceof Location);
+        assertTrue("Quality control" == test_job3.getPositionType().getValue().toString());
+        assertTrue(true == test_job3.getPositionType() instanceof PositionType);
+        System.out.println(test_job3.getPositionType().getValue().toString());
+        assertTrue("Persistence" == test_job3.getCoreCompetency().toString());
+        assertTrue(true == test_job3.getCoreCompetency() instanceof CoreCompetency);
+
+
+    }
+
+
+}
